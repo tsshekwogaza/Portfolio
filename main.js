ecf600c2b83af3ce30b9be8bc1945832fb8bad59
Timothy Samuel <timothysamuelshekwogaza@gmail.com>
2025-05-27 05:58:03 +0100

    form update

--- main.js
+++ main.js
@@ -52,43 +52,43 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     }
 })
 
-const contactForm = document.getElementById('contact-form'),
-        contactName = document.getElementById('contact-name'),
-        contactEmail = document.getElementById('contact-email'),
-        contactProject = document.getElementById('contact-project'),
-        contactMessage = document.getElementById('contact-message');
+// const contactForm = document.getElementById('contact-form'),
+//         contactName = document.getElementById('contact-name'),
+//         contactEmail = document.getElementById('contact-email'),
+//         contactProject = document.getElementById('contact-project'),
+//         contactMessage = document.getElementById('contact-message');
 
-const sendEmail = (e) => {
-    e.preventDefault();
-    // Check if the field has a value
-    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
-        // Add and remove color
-        contactMessage.classList.remove('color-blue');
-        contactMessage.classList.add('color-red');
+// const sendEmail = (e) => {
+//     e.preventDefault();
+//     // Check if the field has a value
+//     if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+//         // Add and remove color
+//         contactMessage.classList.remove('color-blue');
+//         contactMessage.classList.add('color-red');
 
-        // show message
-        contactMessage.textContent = 'Write all the input fields 📖';
-    } else {
-        emailjs.sendForm('service_7xo7i0c', 'template_9dz48o2', '#contact-form', 'ICMDxcI-rZSXd9FCH').then(() => {
-                contactMessage.classList.add('color-blue');
-                contactMessage.textContent = 'Message sent ✅';
+//         // show message
+//         contactMessage.textContent = 'Write all the input fields 📖';
+//     } else {
+//         emailjs.sendForm('service_7xo7i0c', 'template_9dz48o2', '#contact-form', 'ICMDxcI-rZSXd9FCH').then(() => {
+//                 contactMessage.classList.add('color-blue');
+//                 contactMessage.textContent = 'Message sent ✅';
 
-                // Remove message after five seconds
-                setTimeout(() => {
-                    contactMessage.textContent = '';
-                }, 5000);   
-            }, (error) => {
-                alert('OOPS! SOMETHING HAS FAILED...', error);
-            })
+//                 // Remove message after five seconds
+//                 setTimeout(() => {
+//                     contactMessage.textContent = '';
+//                 }, 5000);   
+//             }, (error) => {
+//                 alert('OOPS! SOMETHING HAS FAILED...', error);
+//             })
 
-            // to clear the input fields
-            contactName.value = '';
-            contactEmail.value = '';
-            contactProject.value = '';
-    }
-}
+//             // to clear the input fields
+//             contactName.value = '';
+//             contactEmail.value = '';
+//             contactProject.value = '';
+//     }
+// }
 
-contactForm.addEventListener('submit', sendEmail);
+// contactForm.addEventListener('submit', sendEmail);
 
 const sections = document.querySelectorAll('section[id]');
 
